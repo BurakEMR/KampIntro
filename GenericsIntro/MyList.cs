74ff63b4b3b47ecf7447d9b2efc9dee5770a3f28
Burak Emir <burakemir@ogr.erbakan.edu.tr>
2021-02-11 17:06:35 +0300

    Add project files.

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items; // 0 eleman bile olsa newlediğimiz zaman oluşturmamız lazım.
+        //constructor
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items; // geçici array: önceki değerleri korumak için.
+            items = new T[items.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
+
+        }
+        public int Length
+        {
+            get { return items.Length; }
+        }
+        public T[] Items
+        {
+            get { return items; }
+        }
+    }   
+        
+}
---
+++ OOP1/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    //snippet 
+    class Product  //özellikleri tanımlarız.
+    {
+        public int  Id { get; set; }
+        public int CategoryId { get; set; }  // referans anahtarları ikinci sıraya yazılır.
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }   
+        public int UnitsInStock { get; set; }
+        
+    }
+}
