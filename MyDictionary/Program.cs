f3ef5f2b27f8fc94b6010199d1ea4871a8ff4f10
Burak Emir <burakemir@ogr.erbakan.edu.tr>
2021-02-14 16:05:57 +0300

    Add Projected

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> car = new MyDictionary<string>();
+            car.Add("Jaguar");
+            car.Add("Bmw");
+            car.Add("Mercedes");
+            car.Add("Corvette");
+            Console.WriteLine("MyDicitonary"+ " " +car.Count+ " " + "elemanlıdır.");
+
+        }
+    }
+   
+    
+}
